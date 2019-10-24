ad3d426a603e6a5283541784d0540211a95fa4e1
amaddatu <amaddatu@gmail.com>
2019-10-24 10:43:29 -0400

    Add text indicator function

--- scripts/display-active.js
+++ scripts/display-active.js
@@ -27,8 +27,34 @@ var detectWindowSize = () => {
     });
     return detected_sizes;
 };
+var applyTextIndication = (text, template, elem, selector) => {
+    var output = $(template).get(0).outerHTML.replace("??text??", text);
+    if(typeof elem !== 'undefined'){
+        var found = false;
+        if(typeof selector !== 'undefined'){
+            var finder = $(elem).find(selector);
+            if(finder.length > 0){
+                found = true;
+                finder.html(text);
+            }
+        }
+
+        if(!found){
+            $(elem).append($.parseHTML(output));
+        }
+    }
+    return output;
+};
 $('div').each( (index, elem, v3) => {
     var detected_sizes = detectWindowSize();
-    console.log(detected_sizes);
-    console.log(elem.classList);
+
+    // console.log(detected_sizes);
+    // console.log(elem.classList);
+    // console.log($('<span>').addClass('tester').text("??test??").get(0).outerHTML);
+    applyTextIndication("test", $('<span>').addClass('tester').text("??text??"), elem, '.tester');
+
+
+    console.log({
+        detected_sizes: detected_sizes
+    })
 });
